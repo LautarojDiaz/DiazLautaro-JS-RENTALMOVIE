981bfed6315c0564b89042ce6e7a0537bbc13e5b
LautarojDiaz <111444781+LautarojDiaz@users.noreply.github.com>
2023-01-24 12:32:36 +0100

    Delete DiazLautaro-JS-RENTALMOVIE directory

--- DiazLautaro-JS-RENTALMOVIE/JS/peliculas.js
+++
@@ -1,6 +0,0 @@
-const peliculas = [{id: 1, imagen: "./MULTIMEDIA/avatar.png", nombre: "AVATAR", precio: 1200},
-                {id: 2, imagen: "", nombre: "TOPGUN", precio: 900},
-                {id: 3, imagen: "", nombre: "VOLVERALFUTURO", precio: 300},
-                {id: 4, imagen: "", nombre: "ENDGAME", precio: 700},
-                {id: 5, imagen: "", nombre: "TORRENTE", precio: 150},]
-console.log(peliculas)
